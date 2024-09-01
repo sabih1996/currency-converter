3a27614b1a0e75129dac826954c124d512ace693
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-01 16:15:18 +0300

    added controller method to generate csrf token and also write its module file

--- src/modules/currency/currency.controller.ts
+++ src/modules/currency/currency.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
 
 @Controller('currency')
-export class CurrencyController {}
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Get('csrf-token')
+  async getCsrfToken() {
+    return await this.currencyService.generateToken();
+  }
+}
--- src/modules/currency/currency.module.ts
+++ src/modules/currency/currency.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
+import { CurrencyController } from './currency.controller';
+import { CacheService } from '../cache/cache.service';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [CurrencyController],
+  providers: [CurrencyService, CacheService],
+  exports: [CurrencyService],
+})
 export class CurrencyModule {}
