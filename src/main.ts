8c53e3d73bdc0c1ff8b8221d17d7f3f6204266e5
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-01 16:14:10 +0300

    added swagger for api documentation of backend app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Currency Converter')
+    .setDescription('Currency Converter API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
