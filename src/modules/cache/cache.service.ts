e8a22ab537cc77f636538cb4bf5939db23c2bfa7
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-01 12:35:26 +0300

    update cache service

--- src/modules/cache/cache.service.ts
+++ src/modules/cache/cache.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { RedisClientType, createClient } from 'redis';
 import * as dotenv from 'dotenv';
+import { BadRequestException } from '../../common/error/exception.service';
 
 dotenv.config();
 
@@ -14,19 +15,18 @@ export class CacheService implements OnModuleInit {
       password: process.env.REDIS_PASSWORD,
     });
 
-    this.redisClient.on('error', (err) =>
-      console.error('Redis Client Error', err),
-    );
+    this.redisClient.on('error', () => {
+      throw BadRequestException('Redis client error');
+    });
 
     await this.redisClient.connect();
   }
 
-  async get(key: string): Promise<number | null> {
-    const value = await this.redisClient.get(key);
-    return value ? parseFloat(value) : null;
+  async get(key: string): Promise<string | null> {
+    return await this.redisClient.get(key);
   }
 
-  async set(key: string, value: number): Promise<void> {
-    await this.redisClient.set(key, value.toString(), { EX: 3600 });
+  async set(key: string, value: string): Promise<void> {
+    await this.redisClient.set(key, value, { EX: 3600 });
   }
 }
