c9e485174c277faa349922b6dff807ebd76162c6
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-01 15:23:02 +0300

    Added method generate Token implemented it using it's spec file and test it

--- src/modules/currency/currency.service.spec.ts
+++ src/modules/currency/currency.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrencyService } from './currency.service';
+import { CacheService } from '../cache/cache.service';
 
 describe('CurrencyService', () => {
-  let service: CurrencyService;
+  let currencyService: CurrencyService;
+  let cacheService: CacheService;
 
   beforeEach(async () => {
+    // Create a mock CacheService
+    const cacheServiceMock = {
+      set: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrencyService],
+      providers: [
+        CurrencyService,
+        { provide: CacheService, useValue: cacheServiceMock },
+      ],
     }).compile();
 
-    service = module.get<CurrencyService>(CurrencyService);
+    currencyService = module.get<CurrencyService>(CurrencyService);
+    cacheService = module.get<CacheService>(CacheService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should generate a CSRF token(32 bytes = 64 hex characters), store it in cache, and return it', async () => {
+    const result = await currencyService.generateToken();
+
+    expect(result.csrfToken).toBeDefined();
+    expect(result.csrfToken).toHaveLength(64);
+    expect(cacheService.set).toHaveBeenCalledWith(
+      'csrf-token',
+      result.csrfToken,
+    );
   });
 });
--- src/modules/currency/currency.service.ts
+++ src/modules/currency/currency.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { randomBytes } from 'crypto';
+import { CacheService } from '../cache/cache.service';
+import { GenerateToken } from '../../common/interfaces/currency.interface';
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+  constructor(private readonly cacheService: CacheService) {}
+
+  async generateToken(): Promise<GenerateToken> {
+    const csrfToken = randomBytes(32).toString('hex');
+    await this.cacheService.set('csrf-token', csrfToken);
+    return { csrfToken };
+  }
+}
