b4f65cef8debeb75c7c3f2396352894db3262d52
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-03 21:27:03 +0300

    bind controller method with currency converter bussiness logic

--- src/modules/currency/currency.controller.ts
+++ src/modules/currency/currency.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
+import { CsrfGuard } from '../security/csrf.guard';
+import { CurrencyDTO } from './dto/currency.dto';
 
 @Controller('currency')
 export class CurrencyController {
@@ -8,5 +10,11 @@ export class CurrencyController {
   @Get('csrf-token')
   async getCsrfToken() {
     return await this.currencyService.generateToken();
+  }
+
+  @Get('converter')
+  @UseGuards(CsrfGuard)
+  async currencyConverter(@Query() currencyDto: CurrencyDTO) {
+    return await this.currencyService.currencyConverter(currencyDto);
   }
 }
