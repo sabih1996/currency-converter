4409763340950eb4dcf5a651389930a455e1267f
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-03 20:38:07 +0300

    added initial test cases for currency converter function

--- src/modules/currency/currency.service.spec.ts
+++ src/modules/currency/currency.service.spec.ts
@@ -3,15 +3,23 @@ import { CurrencyService } from './currency.service';
 import { CacheService } from '../cache/cache.service';
 import { SwopManagerService } from './managers/swop/swop.manager.service';
 import * as crypto from 'crypto'; // Import the crypto library to mock
+import { CurrencyDTO } from './dto/currency.dto';
+import { mockCurrenciesList } from '../../../test/data';
+import { BadRequestException } from '../../common/error/exception.service';
 
 describe('CurrencyService', () => {
   let currencyService: CurrencyService;
   let cacheService: CacheService;
+  let swopManagerService: SwopManagerService;
 
   beforeEach(async () => {
     // Create a mock CacheService
     const cacheServiceMock = {
       set: jest.fn(),
+    };
+
+    const swopManagerServiceMock = {
+      fetchCurrencies: jest.fn(),
     };
 
     // Mock the crypto.randomBytes function
@@ -22,13 +30,14 @@ describe('CurrencyService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CurrencyService,
-        { provide: SwopManagerService, useValue: {} },
+        { provide: SwopManagerService, useValue: swopManagerServiceMock },
         { provide: CacheService, useValue: cacheServiceMock },
       ],
     }).compile();
 
     currencyService = module.get<CurrencyService>(CurrencyService);
     cacheService = module.get<CacheService>(CacheService);
+    swopManagerService = module.get<SwopManagerService>(SwopManagerService);
   });
 
   describe('Generate token', () => {
@@ -41,6 +50,39 @@ describe('CurrencyService', () => {
         'csrf-token',
         result.csrfToken,
       );
+    });
+  });
+
+  describe('currencyConverter', () => {
+    it('it should throw a BadRequestException if the source currency is invalid', async () => {
+      (swopManagerService.fetchCurrencies as jest.Mock).mockResolvedValue(
+        mockCurrenciesList,
+      );
+
+      const currencyDto: CurrencyDTO = {
+        sourceCurrency: 'INVALID',
+        targetCurrency: 'EUR',
+        amount: 100,
+      }; // Invalid source currency
+
+      await expect(
+        currencyService.currencyConverter(currencyDto),
+      ).rejects.toThrow(BadRequestException('Currency is not valid'));
+    });
+
+    it('it should throw a BadRequestException if the target currency is invalid', async () => {
+      (swopManagerService.fetchCurrencies as jest.Mock).mockResolvedValue(
+        mockCurrenciesList,
+      );
+      const currencyDto: CurrencyDTO = {
+        sourceCurrency: 'USD',
+        targetCurrency: 'INVALID',
+        amount: 100,
+      };
+
+      await expect(
+        currencyService.currencyConverter(currencyDto),
+      ).rejects.toThrow(BadRequestException('Currency is not valid'));
     });
   });
 });
--- src/modules/currency/currency.service.ts
+++ src/modules/currency/currency.service.ts
@@ -2,13 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { randomBytes } from 'crypto';
 import { CacheService } from '../cache/cache.service';
 import { GenerateToken } from '../../common/interfaces/currency.interface';
+import { CurrencyDTO } from './dto/currency.dto';
+import { SwopManagerService } from './managers/swop/swop.manager.service';
+import { BadRequestException } from '../../common/error/exception.service';
+// import { CurrenciesList } from './managers/swop/interfaces/swop.manager.interface';
 @Injectable()
 export class CurrencyService {
-  constructor(private readonly cacheService: CacheService) {}
+  constructor(
+    private readonly cacheService: CacheService,
+    private readonly swopManagerService: SwopManagerService,
+  ) {}
 
   async generateToken(): Promise<GenerateToken> {
     const csrfToken = randomBytes(32).toString('hex');
     await this.cacheService.set('csrf-token', csrfToken);
     return { csrfToken };
+  }
+
+  async currencyConverter(currencyDto: CurrencyDTO): Promise<any> {
+    const { sourceCurrency, targetCurrency } = currencyDto;
+    await Promise.all([
+      this.validateInputCurrencies(sourceCurrency),
+      this.validateInputCurrencies(targetCurrency),
+    ]);
+  }
+
+  private async validateInputCurrencies(currency: string) {
+    const validCurrency = (
+      await this.swopManagerService.fetchCurrencies()
+    ).some((value) => value.code === currency);
+
+    if (!validCurrency) throw BadRequestException('Currency is not valid');
   }
 }
