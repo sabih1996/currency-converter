2dc0f3fb1f00f2b02816b068f3f7993b04710043
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-09-03 12:31:31 +0300

    update app and currency modules according to new services and manager service changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CurrencyModule } from './modules/currency/currency.module';
-import { CurrencyService } from './modules/currency/currency.service';
-import { CurrencyController } from './modules/currency/currency.controller';
-import { CacheService } from './modules/cache/cache.service';
 
 @Module({
   imports: [CurrencyModule],
-  controllers: [AppController, CurrencyController],
-  providers: [AppService, CurrencyService, CacheService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/modules/currency/currency.module.ts
+++ src/modules/currency/currency.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
 import { CurrencyController } from './currency.controller';
 import { CacheService } from '../cache/cache.service';
+import { SwopManagerService } from './managers/swop/swop.manager.service';
 
 @Module({
-  imports: [],
   controllers: [CurrencyController],
-  providers: [CurrencyService, CacheService],
+  providers: [CurrencyService, CacheService, SwopManagerService],
   exports: [CurrencyService],
 })
 export class CurrencyModule {}
